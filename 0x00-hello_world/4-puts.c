1a55e36283aaed6a151ef83f26dc150a28f38521
jacobwatua <jacobwatua254@gmail.com>
2023-02-17 03:35:23 -0800

    Using puts in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -13,6 +13,6 @@
 /*main function*/
 int main(void) 
 {   
-    puts('"Programming is like building a multilingual puzzle');
-    return (0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return(0);
 }
