9f177a59e5099424ec41fdbd80e9237e1e1c2a96
jacobwatua <jacobwatua254@gmail.com>
2023-02-17 09:22:09 -0800

    C variables, if, else, for amd while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -20,15 +20,15 @@ n = rand() - RAND_MAX / 2;
 /* your code goes there */
 if (n > 0)
 {
-    printf("is positive\n");
+printf("is positive\n");
 }
 else if (n == 0)
 {
-    printf("is zero\n");
+printf("is zero\n");
 }
 else
 {
-    printf("is negative\n");
+printf("is negative\n");
 }
 return (0);
 }
