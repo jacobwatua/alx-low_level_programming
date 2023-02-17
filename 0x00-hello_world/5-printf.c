f2391ba696670a06c4139c29ac104eab594930b9
jacobwatua <jacobwatua254@gmail.com>
2023-02-17 04:10:18 -0800

    Using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+ * main function
+ @void -accepts zero arguements
+ *
+ * Description: This function uses pritf to print a string
+ * Return: The function returns 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
