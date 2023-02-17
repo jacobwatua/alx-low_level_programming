85fbdbe748bf511d25837a17ed6ef866f4f6a94f
jacobwatua <jacobwatua254@gmail.com>
2023-02-17 10:21:55 -0800

    Check the last digit of a file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,24 +3,31 @@
 /* more headers goes there */
 #include <stdio.h>
 /* betty style doc for function main goes there */
+/**
+* main - The entry point
+*
+* Description: checks if last digit is < 5 , or > 5, or == 0
+* Return: 0 if successful
+*/
 int main(void)
 {
 int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
+n = 980
 /* your code goes there */
 if (((n % 10) == 0))
 {
 printf("Last digit of %d is %d and is 0\n", n, (n % 10));
 }
-if (((n % 10) >  5))
+else if (((n % 10) >  5))
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, (n % 10));
 }
 else
 {
-printf("Last digit of %d is %d  and is less than 6 and not 0\n", n, (n % 10));
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, (n % 10));
 }
 return (0);
 }
