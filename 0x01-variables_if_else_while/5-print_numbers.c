2612b8f0c436f6045b33b340d2270ec5a7d063da
jacobwatua <jacobwatua254@gmail.com>
2023-02-17 11:55:01 -0800

    Integers between 0 and 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+ * main - entry point
+ *
+ * Description:  Print numbers with base 10
+ * Return: 0 if successful
+*/
+int main(void)
+{
+int s = 0;
+do {
+printf("%d", s);
+s++;
+} while (s < 10);
+printf("\n");
+}
