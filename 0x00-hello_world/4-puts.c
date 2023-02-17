f5e2eb6fbb337c74e051fde72b3dc694fe6b06c3
jacobwatua <jacobwatua254@gmail.com>
2023-02-17 04:01:18 -0800

    Using puts in c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
-#include  <stdio.h>
-/*
-* main - This is the main function that prints out a statement
-@void : the function accepts no parameter (s)
-*
-*Description : The function prints out the argument passed in the main function
-*Return : The funtion returns zero
+#include <stdio.h>
+/**
+* main-Entry point
+* Return: 0 if successful
 */
 int main(void)
-{   
+{
 puts("\"Programming is like building a multilingual puzzle");
-return(0);
+return (0);
 }
