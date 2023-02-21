20bfb42421028d9595538fbae9c86dfb38d12a5a
jacobwatua <jacobwatua254@gmail.com>
2023-02-21 11:35:46 -0800

    Functions, Nested loops, Recursiveness

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+/**
+* main - The main Entry point
+* @args - accepts no arguements
+*
+* Description: prints _putchar
+* Return: 0 if successful
+*/
+
+int main(void)
+{
+printf("%s", "_putchar\n");
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,8 @@
+This includes lessons on functions, while loops, nested loops, and forloops <br>
+<h1> Topics </h1>
+<ul>
+	<li>While loops </li>
+	<li>For loops </li>
+	<li>Functions </li>
+	<li>Nested loops </li>
+</ul>
