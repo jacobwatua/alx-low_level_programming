675d1293ad8bd7437b0d188c4d4a0af5133546b2
jacobwatua <jacobwatua254@gmail.com>
2023-02-22 06:27:26 -0800

    Using _putchar and header filrs in c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,6 @@
-#include "stdio.h"
+#include "main.h"
+#include <string.h>
+#include <unistd.h>
 /**
 * main - The main Entry point
 * @args - accepts no arguements
@@ -6,9 +8,28 @@
 * Description: prints _putchar
 * Return: 0 if successful
 */
-
 int main(void)
 {
-printf("%s", "_putchar\n");
+char str[] = "_putchar";
+int i;
+int len = strlen(str);
+for (i = 0; i < len; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
 return (0);
+}
+
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
