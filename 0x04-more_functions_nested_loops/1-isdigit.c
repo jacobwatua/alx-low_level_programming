11eeba818c9a522b7070f83e129fd57ad357254e
jacobwatua <jacobwatua254@gmail.com>
2023-02-24 05:18:18 -0800

    Check if a char is an integer

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isdigit - Name of the func
+ * @c: one integer argument passed
+ *
+ * Description: checkcs if a argument is an integer
+ * Return: 1 if true 0 if not true
+*/
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
+
