f16a22f5873c15e7dffb57ecf4a3b6195204ae0c
jacobwatua <jacobwatua254@gmail.com>
2023-02-24 05:52:13 -0800

    Print 0-9 using for loop

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - function name
+ * @void: no args passed
+ *
+ * Descrption: print 0-9
+ * Return: null
+*/
+void print_numbers(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+_putchar(i + '0');
+}
+_putchar('\n');
+}
+
