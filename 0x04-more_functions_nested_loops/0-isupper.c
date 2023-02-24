03fee09108b192de81d9c1d2e725ea1beec53fef
jacobwatua <jacobwatua254@gmail.com>
2023-02-24 04:50:42 -0800

    Check if an character is upper case

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,12 +7,7 @@
  * Description: checks if a letter is uppercase
  * Return: 1 if true 0 if false
 */
-/**
- * _isupper - checks for uppercase character
- * @c: character to check
- *
- * Return: 1 if c is uppercase, 0 otherwise
- */
+
 int _isupper(int c)
 {
 return (c >= 'A' && c <= 'Z');
