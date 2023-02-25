a45e53f106eab540009ea7fbdaf14ceb10f2086e
jacobwatua <jacobwatua254@gmail.com>
2023-02-24 23:05:56 -0800

    print lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
 /**
- * print_line - Draws a straight line in the terminal using the underscore character.
+ * print_line - Draws a straight line in the terminal
+ * using the underscore character.
  *
  * @n: The number of times the underscore character should be printed.
  *
- * This function prints a line of underscores in the terminal n number of times.
+ * This function prints a line of underscores
+ * in the terminal n number of times.
  * After printing the line, it adds a newline character to the end.
  */
 void print_line(int n)
