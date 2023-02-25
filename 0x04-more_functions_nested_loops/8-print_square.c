baee189d4430666fe34d87c2666422d72f36a5c0
jacobwatua <jacobwatua254@gmail.com>
2023-02-24 23:47:50 -0800

    print squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_square - function name
+ * @size: size of the square to be printed
+ *
+ * This function prints square using # sign
+ * Return: no return value
+*/
+void print_square(int size)
+{
+int i,j;
+if (size <= 0)
+{
+_putchar('\n');
+return;
+}
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+
