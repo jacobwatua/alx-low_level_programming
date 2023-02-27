d42842c83835ea3b55efc1e90f8b4750110ff438
jacobwatua <jacobwatua254@gmail.com>
2023-02-27 09:43:19 -0800

    Using puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - Entry point
+ * @str: string to be printed
+ *
+ * Description: Prints the str argument passed
+ * Return: No return value
+*/
+void _puts(char *str)
+{
+puts(str);
+}
+
