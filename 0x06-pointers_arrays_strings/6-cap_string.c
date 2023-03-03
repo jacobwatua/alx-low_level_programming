4c679353db7b641a54bad8252b8fd8101c9e42ce
jacobwatua <jacobwatua254@gmail.com>
2023-03-03 11:30:08 -0800

    capitalize words in a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,37 +1,43 @@
+
 #include "main.h"
-#include <ctype.h>
-#include <string.h>
+
+/**
+ * _islower - checks if a character is lowercase
+ * @c: character to check
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+static int _islower(int c)
+{
+return (c >= 'a' && c <= 'z');
+}
+
+/**
+ * _isseparator - checks if a character is a separator
+ * @c: character to check
+ * Return: 1 if c is a separator, 0 otherwise
+ */
+static int _isseparator(char c)
+{
+return (c == ' ' || c == '\t' || c == '\n' ||
+c == ',' || c == ';' || c == '.' ||
+c == '!' || c == '?' || c == '"' ||
+c == '(' || c == ')' || c == '{' ||
+c == '}');
+}
 
 /**
  * cap_string - capitalizes all words of a string
  * @s: string to capitalize
  * Return: pointer to string s
  */
-
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
-int len = strlen(str);
 int i;
 
-for (i = 0; i < len; i++)
-{
+for (i = 0; s[i]; ++i)
+if (i == 0 || _isseparator(s[i - 1]))
+(_islower(s[i])) ? s[i] -= 32 : 0;
 
-if (i == 0 || isspace(str[i - 1]) || str[i - 1] == ','
-|| str[i - 1] == ';' || str[i - 1] == '.'
-|| str[i - 1] == '!' || str[i - 1] == '?'
-|| str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')'
-|| str[i - 1] == '{' || str[i - 1] == '}')
-{
-str[i] = toupper(str[i]);
+return (s);
 }
-
-else
-{
-str[i] = tolower(str[i]);
-}
-}
-
-return (str);
-}
-
 
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,5 +9,7 @@
     void reverse_array(int *a, int n);
     char *string_toupper(char *s);
     char *cap_string(char *str);
+    static int _islower(int c);
+static int _isseparator(char c);
 #endif
 
