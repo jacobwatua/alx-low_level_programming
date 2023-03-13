99728e88a04ce5e7347a45d7d94c5fde12688fae
jacobwatua <jacobwatua254@gmai.com>
2023-03-13 17:05:52 +0300

    updates and factorials

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * factorial - computes factorial of a number
+ * @n: number to be computed
+ *
+ * Return: The factorial of arguement passed
+*/
+int factorial(int n)
+{
+if (n < 0) {
+    return (-1);
+}
+else if (n == 0 || n == 1)
+{
+return (1);
+}
+else {
+return (n * factorial(n - 1));
+}
+}
