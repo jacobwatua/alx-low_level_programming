c3f3f347cb14fff9d9cde32623ec0d5d043cc7dc
jacobwatua <jacobwatua254@gmail.com>
2023-09-02 05:36:40 +0300

    fix put char

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,6 +1,20 @@
 #include "main.h"
+#include <stdio.h>
 
+/**
+* _puts - Prints a string to stdout followed by a newline.
+* @s: The string to be printed.
+*
+* Return: No return value.
+*/
 void _puts(char *s)
 {
-
+if (s != NULL)
+{
+while (*s)
+{
+putchar(*s);
+s++;
+}
+}
 }
