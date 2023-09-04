0691862147642b43391c41f7736423b96438ba14
jacobwatua <jacobwatua254@gmail.com>
2023-09-04 10:34:50 +0300

    Corrections

--- 0x09-static_libraries/README.md
+++ 0x09-static_libraries/README.md
@@ -1,1 +1,3 @@
-C static libs
+<h1>Static libraries</h1>
+- Creating static C libraries 
+- Using static libraries in c programs
--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,5 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _putchar(char c);
 int _islower(int c);
@@ -21,4 +21,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
 #endif
