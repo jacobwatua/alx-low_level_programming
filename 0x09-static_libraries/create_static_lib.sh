825874a8a8ef5a90519a75b4aff0c011aedd17e1
jacobwatua <jacobwatua254@gmail.com>
2023-09-04 10:41:35 +0300

    attempt 2

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -c *.c
-ar -rc liball.a *.o
-ranlib liball.a
+
+
+output_library="liball.a"
+
+
+c_sources=$(find . -maxdepth 1 -name "*.c")
+
+
+if [ -z "$c_sources" ]; then
+  echo "No .c files found in the current directory."
+  exit 1
+fi
+
+
+for source in $c_sources; do
+  gcc -Wall -pedantic -Werror -Wextra -c "$source"
+done
+
+
+ar -rc "$output_library" *.o
+
+
+ranlib "$output_library"
+
+echo "Static library $output_library created successfully."
