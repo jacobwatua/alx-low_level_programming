97917b474d209d2d9368310f1faa372b03e97ec3
jacobwatua <jacobwatua254@gmail.com>
2023-09-14 05:46:29 +0300

    Print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints a name
+ * @name: stirng to be printed
+ * @f: print name callback function
+ *
+ * Return: no return value
+*/
+void print_name(char *name, void (*f)(char *))
+{
+if (name && f)
+{
+    f(name);
+}
+}
