3807e0c8d00120a0ec39da12d81d678e9b9d0b6f
jacobwatua <jacobwatua254@gmail.com>
2023-12-11 20:16:39 +0300

    Dynamic lib.sh

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,9 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
 
 int _putchar(char c);
 int _islower(int c);
@@ -25,4 +24,10 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#endif
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int mod(int a, int b);
+extern div_t div(int __numer, int __denom);
+
+#endif /* MAIN_H */
